2c624767c5cd85ed587825a87debc7800c23e1fe
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-09-28 10:26:04 +0000

    test if the login works correctly

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <h1 className="text-3xl">LMS</h1>;
+  return (
+    <h1 className="text-3xl">
+      LMS
+      <UserButton />
+    </h1>
+  );
 }
