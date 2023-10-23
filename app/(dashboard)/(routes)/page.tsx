c26f3de26063c62d4fc78c45be6d680e798e8f21
Zaid-maker <pzhafeez@gmail.com>
2023-10-23 20:34:14 +0500

    finally start working on this project

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <h1 className="text-3xl">
-      LMS
+      Learning Management System
       <UserButton afterSignOutUrl="/" />
     </h1>
   );
