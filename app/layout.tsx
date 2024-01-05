1e0e0d110bd58d29922510ad001d21466318cba0
Zaid-maker <pzhafeez@gmail.com>
2024-01-05 19:40:47 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/webhook"],
+});
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
